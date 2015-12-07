cb6737789ff16ad1b6d7f691c871680aab9d68d9
B50calabrese <antoniobrese@gmail.com>
2015-12-07 16:37:30 -0500

    Update TestCV.py

--- src/python/TestCV.py
+++ src/python/TestCV.py
@@ -3,25 +3,32 @@ import cv2
  
 vc = cv2.VideoCapture(0)
 
-WIDTH = 640
-HEIGHT = 480
-
 def getImg():
   _,i = vc.read()
-  #res = cv2.resize(i,[WIDTH, HEIGHT], fx = .5, fy = .5, interpolation = cv2.INTER_AREA)
   return i
 
+# Used to grab only the green pixels in an HSV image.
 lowerArray = np.array([50, 100, 100], np.uint8)
 upperArray = np.array([70, 255, 255], np.uint8)
 
 img = getImg()
 
 while 1 > 0:
+    # Blur image in order to reduce noise.
+    blur = cv2.blur(img, (5,5))
+ 
     print "Converting to green"
-    greenImg = cv2.inRange(img, lowerArray, upperArray)
+    
+    # Converts the image to HSV then grabs only the green.
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    greenImg = cv2.inRange(hsv, lowerArray, upperArray)
+    
     print "Finding contours"
+    
     contours,h = cv2.findContours(greenImg, 1, 2)
+    
     print "Printing contours"
+    
     for cnt in contours:
       approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
       if len(approx) == 4:
