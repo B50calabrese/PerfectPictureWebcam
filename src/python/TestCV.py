e232e90c3df8f261339f4bb1e1579666cdfd7f33
B50calabrese <antoniobrese@gmail.com>
2015-12-07 12:34:57 -0500

    Update TestCV.py

--- src/python/TestCV.py
+++ src/python/TestCV.py
@@ -1,30 +1,25 @@
 import numpy as np
 import cv2
  
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 vc = cv2.VideoCapture(0)
-rval, img = vc.read()
-font = cv2.FONT_HERSHEY_SIMPLEX
 
-if rval == False:
-    exit()
+WIDTH = 640
+HEIGHT = 480
+
+def getImg():
+  _,i = vc.read()
+  res = cv2.resize(i,[WIDTH, HEIGHT], fx = .5, fy = .5, interpolation = cv2.INTER_AREA)
+  return res
 
 while 1 > 0:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-
-    # Puts text on the image, the text being "Test". The position is (10, 500).
-    # The text size is 4. The color is white. The scale is 2, and always use
-    # cv2.LINE_AA.
-    cv2.putText(img, "Test", (10,500), font, 4, (255,255,255), 2, cv2.LINE_AA)
-    cv2.imshow('img',img)
-    rval, img = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
-cv2.destroyAllWindows()
+    ret,threshold = cv2.threshold(gray, 127, 255, 1)
+    print "Finding contours"
+    contours,h = cv2.findContours(threshold, 1, 2)
+    print "Printing contours"
+    for cnt in contours:
+      approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+      if len(approx) == 4:
+        print "Found square!!!!", cnt
+    img = getImg()
 vc.release()
